f2b390fccd4a63cc7dea0152bf0ed406fafe4ace
daphtya <davidpanjaitan1@gmail.com>
2019-10-23 17:06:59 +0700

    added tictactoe

---
+++ games/tictactoe/precache-manifest.4524d1afeb95d52003754115fcc8a6c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e978b356371c7078118fdc47935252e",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "dcb56fb86ed12d09b825",
+    "url": "/react/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/react/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "dcb56fb86ed12d09b825",
+    "url": "/react/static/js/main.d350e48b.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  }
+]);
